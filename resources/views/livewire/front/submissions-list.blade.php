3486747b0061e0f9986ca3c584228695b2c3976c
jatinbphp <jatin.b.php@gmail.com>
2025-01-13 13:21:39 +0530

    listen and vote design changes

--- resources/views/livewire/front/submissions-list.blade.php
+++ resources/views/livewire/front/submissions-list.blade.php
@@ -1,16 +1,35 @@
 <div class="song-list-left">
+    <div class="song-list-scroll">
     <ul>
         @if(!empty($submissionsList))
             @foreach ($submissionsList as $value)
                 <li>
-                    <button class="play-button" title="View Video" data-id="{{ $value->id }}" data-url="{{ route('submission.video', ['id' => $value->id]) }}">
-                        <img src="{{url('assets/dist/front/img/play-button-white.svg') }}" alt="" />
-                    </button>
-                    <div class="song-name">
-                        <h6>{{ $value->submissionTitle }}</h6>
-                        <p>{{ $value->fullName }}</p>
+                    <div class="left">
+                        <div class="icon">
+                            <button class="play-button" title="View Video" data-id="{{ $value->id }}" data-url="{{ route('submission.video', ['id' => $value->id]) }}">
+                                <img src="{{url('assets/dist/front/img/play-button-white.svg') }}" alt="" />
+                            </button>
+                            <button class="play-button">
+                                <img src="{{url('assets/dist/front/img/play-button-white.svg') }}" alt="" />
+                            </button>
+                        </div>
+                        <div class="song-name">
+                            <h6>{{ $value->submissionTitle }}</h6>
+                            <p>{{ $value->fullName }}</p>
+                        </div>
                     </div>
-                    <button class="vote-button" data-vote-id="{{ $value->id }}">VOTE</button>
+                    <div class="right">
+                        <div class="song-progress">
+                            <div id="progress_bar_box">
+                                <div id="progress_bar"></div>
+                                <div id="progress"></div>
+                            </div>
+                        </div>
+                        <div class="song-btn">
+                            <button><img src="{{url('assets/dist/front/img/like-icon.png') }}" alt="" /></button>
+                            <button><img src="{{url('assets/dist/front/img/upload-free-icon.png') }}" alt="" /></button>
+                        </div>
+                    </div>
                 </li>
             @endforeach
         @else
@@ -19,6 +38,7 @@
             </li>
         @endif
     </ul>
+</div>
     @if(!empty($submissionsList))
         <input type="hidden" id="submission-add-vote" value="{{ route('submission.add.vote') }}">
         
