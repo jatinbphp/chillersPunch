9db7e11a4981b58b9db49b421ffe1470a8dd800b
Jatin Bhatt <jatin.b.php@gmail.com>
2025-01-17 12:57:52 +0530

    pagination changes

--- app/Livewire/Front/SubmissionsList.php
+++ app/Livewire/Front/SubmissionsList.php
@@ -8,7 +8,7 @@ use App\Models\Submission;
 class SubmissionsList extends Component
 {
     public $submissionsList = [];
-    public $totalVisible = 6;
+    public $totalVisible = 7;
     public $isChartsPage = false;
     public $isFinalistPage = false;
 
@@ -27,7 +27,7 @@ class SubmissionsList extends Component
     }
 
     public function loadMore(){
-        $this->totalVisible += 6;
+        $this->totalVisible += 7;
         $this->fetchSubmissions();
     }
 
