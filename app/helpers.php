05d4344dd893ae9e6dc7a9b6c744d077a755949d
Jatin Bhatt <jatin.b.php@gmail.com>
2025-01-24 16:15:13 +0530

    changes

--- app/helpers.php
+++ app/helpers.php
@@ -5,8 +5,43 @@ use App\Models\Submission;
 use App\Models\Competition;
 
 if (!function_exists('formatCreatedAt')) {
-    function formatCreatedAt($createdAt){
+    function formatCreatedAt($createdAt)
+    {
         return Carbon::parse($createdAt)->format('Y-m-d H:i:s');
+    }
+}
+
+if (!function_exists('getHomeUrl')) {
+    function getHomeUrl()
+    {
+        return route('home');
+    }
+}
+
+if (!function_exists('getRoleWiseHomeUrl')) {
+    function getRoleWiseHomeUrl()
+    {
+        if(!Auth::user()){
+            return '';
+        }
+        return route('dashboard');
+    }
+}
+
+if (!function_exists('getRoleWiseHomeLabel')) {
+    function getRoleWiseHomeLabel()
+    {
+        if(!Auth::user()){
+            return '';
+        }
+        return 'Dashboard';
+    }
+}
+
+if (!function_exists('getYears')) {
+    function getYears()
+    {
+        return ['2019', '2020', '2021', '2022', '2023'];
     }
 }
 
